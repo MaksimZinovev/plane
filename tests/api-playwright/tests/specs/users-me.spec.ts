6c4306c21d562542ba84ea0c05b702286cb8c9f0
Maksim Zinovev <maksimzinovevsubmit@gmail.com>
2023-09-27 18:46:10 +1000

    Add GET users-me test

--- tests/api-playwright/tests/specs/users-me.spec.ts
+++ tests/api-playwright/tests/specs/users-me.spec.ts
@@ -5,11 +5,13 @@ import { test, expect } from "@playwright/test";
 test("GET users-me", async ({ request }) => {
     const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk2NDA0ODc5LCJpYXQiOjE2OTU4MDAwNzksImp0aSI6Ijk1YThkYzZjZmNjNjRmNDBhZWU4ZTI4OTk0Nzk0Y2ZhIiwidXNlcl9pZCI6IjRlN2RhZDAxLTI4ZDktNGFjMy1iOGZlLWE2NTE0YTMwODNkOCJ9.MS__mkYnhmH1lgRLdl_IEeqLkUG_X2kx9UUeRzVSlYM'
     const headers = {
-        headers: { Authorization: TOKEN }
+        headers: { Authorization: `Bearer ${TOKEN}` }
     }
-    const response = await request.get('api/api/users/me', headers)
+    const response = await request.get("api/users/me", headers)
     expect(response.status()).toBe(200);
 
     const body = await response.json();
+    // console.log(body);
+    
 
 });
